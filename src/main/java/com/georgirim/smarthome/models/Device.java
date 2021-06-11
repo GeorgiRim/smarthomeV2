d00b584d8eb162054793668b1dce206d426b7b8e
Georgi <rimargeorg40@gmail.com>
2021-06-11 22:22:35 +0500

    1

---
+++ src/main/java/com/georgirim/smarthome/models/Device.java
@@ -0,0 +1,80 @@
+package com.georgirim.smarthome.models;
+
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+
+import javax.persistence.*;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Entity
+public class Device {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "ID", updatable = false, nullable = false)
+    private int id;
+
+    private String ico;
+
+    private String ip;
+
+    //@Enumerated(value = EnumType.STRING)
+    private int type;
+
+    private DeviceData deviceData;
+
+    public Device(){}
+
+    public Device(int type, String ip){
+
+        if(ico == null){
+            switch (type){
+                case 1:ico = "https://e7.pngegg.com/pngimages/874/595/png-clipart-smoke-detector-fire-alarm-control-panel-conflagration-fire-protection-smoke-smoke-smoke-detector-thumbnail.png"; break;
+                case 2:ico = "https://www.pngfind.com/pngs/m/136-1367874_web-camera-png-free-download-webcam-png-transparent.png"; break;
+                case 3:ico = "https://i.pinimg.com/originals/8d/cd/59/8dcd596c75a0f2629e7493ea575c8b2a.jpg"; break;
+                case 4:ico = "https://i.pinimg.com/originals/8d/cd/59/8dcd596c75a0f2629e7493ea575c8b2a.jpg"; break;
+                default: ico = "https://i.pinimg.com/originals/8d/cd/59/8dcd596c75a0f2629e7493ea575c8b2a.jpg";
+            }
+            //this.type = type;
+            this.ip = ip;
+        }
+    }
+
+    public enum Type{
+        FIRE,
+        TEPERATURE,
+        CAM,
+        SUNSET
+    }
+
+    public String getIco() {
+        return ico;
+    }
+
+    public void setIco(String ico) {
+        this.ico = ico;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
