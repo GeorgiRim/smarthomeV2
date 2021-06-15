a4de4df131750f17e8a90f3a6ada266733f90d4a
GeorgiRim <48623601+GeorgiRim@users.noreply.github.com>
2021-06-15 09:01:43 +0500

    TEST

---
+++ src/main/java/com/georgirim/smarthome/models/DeviceCam.java
@@ -0,0 +1,9 @@
+package com.georgirim.smarthome.models;
+
+import com.georgirim.smarthome.models.Device;
+
+public class DeviceCam extends Device {
+    public DeviceCam(Device.Type type, String mac) {
+        super(type, mac);
+    }
+}
